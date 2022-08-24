c5bd21d524c8ac8bfa32b093aa9292f7ddf91ba9
kelmorsy <khaled.mg.a@gmail.com>
2022-08-24 18:01:34 +0200

    Add data and controller model

---
+++ src/model/controller.js
@@ -0,0 +1,23 @@
+import data from './data'
+const controller = function(model) {
+
+  const clone = (object) => JSON.parse(JSON.stringify(object))
+
+  async function getAll() {
+    return clone(model);
+  }
+
+  async function getFiltered(callback) {
+    return clone(model.filter(callback))
+  }
+
+  async function setField(id, field, newValue) {
+    const item = model.find(item => item.id === id);
+    if (!item[field]) throw new Error('Field does\'t exist');
+    item[field] = newValue;
+  }
+
+  return { getAll, getFiltered, setField };
+}(data)
+
+export default controller
--- src/model/data.js
+++ src/model/data.js
@@ -3,37 +3,37 @@ const items = [
     id: 1,
     name: 'Tuna',
     price: 5,
-    quantity: 5,
+    stock: 5,
   },
   {
     id: 2,
     name: 'Turkey',
     price: 8,
-    quantity: 6,
+    stock: 6,
   },
   {
     id: 3,
     name: 'Lobster Roll',
     price: 15,
-    quantity: 4,
+    stock: 4,
   },
   {
     id: 4,
     name: 'Egg Salad',
     price: 4,
-    quantity: 8,
+    stock: 8,
   },
   {
     id: 5,
     name: 'Roast Beef',
     price: 10,
-    quantity: 6,
+    stock: 6,
   },
   {
     id: 6,
     name: 'Smoked Salmon',
     price: 12,
-    quantity: 5,
+    stock: 5,
   },
 ];
 
